44bebdbf7563dcb523b590c988f4d235cb3b1dd1
Gus Knowlden <gus@heavyset.io>
2019-02-01 11:23:58 -0500

    Updates config.mk to find glib deps

--- config.mk
+++ config.mk
@@ -16,10 +16,12 @@ GTKINC = `pkg-config --cflags gtk+-3.0 gcr-3 webkit2gtk-4.0`
 GTKLIB = `pkg-config --libs gtk+-3.0 gcr-3 webkit2gtk-4.0`
 WEBEXTINC = `pkg-config --cflags webkit2gtk-4.0 webkit2gtk-web-extension-4.0`
 WEBEXTLIBS = `pkg-config --libs webkit2gtk-4.0 webkit2gtk-web-extension-4.0`
+GLIBINC = `pkg-config --cflags glib-2.0`
+GLIBLIB = `pkg-config --libs glib-2.0`
 
 # includes and libs
-INCS = $(X11INC) $(GTKINC)
-LIBS = $(X11LIB) $(GTKLIB) -lgthread-2.0
+INCS = $(X11INC) $(GTKINC) $(GLIBINC)
+LIBS = $(X11LIB) $(GTKLIB) $(GLIBLIB) -lgthread-2.0
 
 # flags
 CPPFLAGS = -DVERSION=\"$(VERSION)\" -DWEBEXTDIR=\"$(LIBDIR)\" \
